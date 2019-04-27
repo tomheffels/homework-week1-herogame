8825001fe6357277cce013a98c0d73a0a6e3852d
Tom Heffels <tomheffels@hotmail.com>
2019-04-27 13:16:54 +0200

    made it look nice(r)

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -14,19 +14,23 @@ function rest(hero) {
     if (hero.health === 10) {
         alert("No need to rest now, " + hero.name + "! Your health is still full...")
     }
+    /* else {
+           alert(`Good morning ${hero.name}, your health is fully replenished! Health: ${hero.health}`)
+       } */
     hero.health = 10;
-    console.log(`Good morning ${hero.name}, your health is fully replenished! Health: ${hero.health}`)
+    console.log(`Health: ${hero.health}`)
     return hero;
 };
 
 function pickUpItem(hero, weapon) {
     if (weapon === 'dagger') {
         weapon = {
-            type: "dagger",
+            type: 'dagger',
             damage: 2
         }
     }
     hero.inventory.push(weapon)
+        // alert(`A ${weapon.type} was added to your inventory.`)
     console.log(`A ${weapon.type} was added to your inventory.`)
     console.log(hero.inventory)
         // document.getElementById('dagger').remove();
@@ -38,12 +42,13 @@ function equipWeapon(hero) {
     } else {
         hero.weapon = hero.inventory[hero.inventory.length - 1]
     }
+    // alert(`You equipped your ${hero.weapon.type}.`)
     console.log(`You equipped your ${hero.weapon.type}.`)
     console.log(hero.weapon)
 }
 
 function displayStats(hero) {
-    document.write("YOUR HERO ");
+    document.writeln("YOUR HERO ");
     document.writeln("name: " + hero.name.toUpperCase());
     document.writeln("health: " + hero.health + "HP");
     document.writeln("weapon type: " + hero.weapon.type.toUpperCase());
@@ -62,6 +67,10 @@ function setHeroName(hero) {
     }
 }
 
+document.getElementById("changeHeroName").onclick = function() {
+    setHeroName(hero);
+}
+
 function fightMonster(hero) {
     alert(`A monster appeared right in front of you, ${hero.name}, time to fight!`)
     hero.health = (hero.health - 3);
@@ -72,9 +81,5 @@ function fightMonster(hero) {
     document.getElementById('monster').remove();
 }
 
-
-document.getElementById("changeHeroName").onclick = function() {
-    setHeroName(hero);
-}
-
+console.log(hero)
 displayStats(hero)
